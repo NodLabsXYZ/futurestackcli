035fb0921e49c352f299fe7aa760ecc140f3b46c
Jared Cosulich <jared.cosulich@gmail.com>
2022-03-05 14:02:34 -0500

    uploading description

--- src/getDescription.js
+++ src/getDescription.js
@@ -2,13 +2,19 @@ import fs from 'fs';
 import path from 'path';
 
 const getDescription = async () => {
-  console.log("URL", import.meta.url)
   const appDir = path.dirname(import.meta.url);
-  console.log("APPDIR: ", appDir);
+  const pathParts = appDir.split(path.sep);
+  const index = pathParts.indexOf('node_modules');
+  const rootDir = pathParts.slice(0, index).join(path.sep);
+  console.log("FULL", path.join(rootDir, 'contract.md'))
+  const decriptionPath = path.join(rootDir, 'contract.md').replace('file:', '');
+  console.log("path", decriptionPath)
   
   try {
-    return fs.readFileSync('', 'utf8')
+    console.log(fs.readFileSync(decriptionPath, 'utf8'))
+    return fs.readFileSync(decriptionPath, 'utf8')
   } catch (err) {
+    console.log("ERROR", err)
     return null;
   }
 }
